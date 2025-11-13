decb38f9dfbe270f4f70bfdabe80921a87dd0f56
Iryna9105 <irina.grosul9105@gmail.com>
2025-11-13 11:19:23 +0200

    Adding tests to SeleniumTestngTest.java class and new document README_TEST.md

--- README_TEST.md
+++ README_TEST.md
@@ -1,13 +1,35 @@
 Початок виконання тестування:
 1. На комп'ютері має бути встановлено: IntelliJ IDEA,JAVA, MAVEN, SELENIUM, TESTNG.
-2. Зклонуй репозиторій https://github.com/JAVA-QA-AUTO-UA-RB/12-webui-testing-framework-VolodymyrOstrikov.git
+2. Зклонуй відповідний репозиторій 
 3. Відкрий його в IntelliJ IDEA.
 4. Знайди файл `SeleniumTestngTest.java`.
-5. Виконай Run`SeleniumTestngTest`.
-6. Очікований результат виконання тесту: ![img_1.png](img_1.png).
+5. Переконайся, що всі тести створені.
+   Список тестів:
+    - abTestingPageHasSpecificTextTest — перевірка заголовку A/B Testing сторінки
+    - abTestingPageHasSpecificTextTest — перевірка заголовку A/B Testing сторінки
+    - addRemoveElementsTest — додавання та видалення елементів
+    - addCheckboxTest — робота з чекбоксами
+    - dropdownSelectionTest — робота з випадаючим списком
+    - loginLogoutTest — перевірка логіну та логауту
+    - dragAndDropElementsTest — Drag & Drop елементів
+    - sliderElementTest — робота з горизонтальним слайдером
+6. Виконай Run`SeleniumTestngTest`.
 
-7.Виконай Maven build за допомогою терміналу, відкрий термінал у папці з проектом та введи:
+7. Виконай Maven build за допомогою терміналу, відкрий термінал у папці з проектом та введи:
    ```bash
    mvn clean test
    ```
-8. Результат успішного Maven build виглядає так: ![img.png](img.png).
+8. Результат успішного Maven build виглядає так:
+   [INFO] Tests run: 7, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 15.10 s -- in TestSuite
+   [INFO]
+   [INFO] Results:
+   [INFO]
+   [INFO] Tests run: 7, Failures: 0, Errors: 0, Skipped: 0
+   [INFO]
+   [INFO] ------------------------------------------------------------------------
+   [INFO] BUILD SUCCESS
+   [INFO] ------------------------------------------------------------------------
+   [INFO] Total time:  22.429 s
+   [INFO] Finished at: 2025-11-13T10:56:53+02:00
+   [INFO] ------------------------------------------------------------------------
+   PS X:\_projects\12-webui-testing-framework-Iryna9105> 
--- src/test/java/SeleniumTestngTest.java
+++ src/test/java/SeleniumTestngTest.java
@@ -1,25 +1,127 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 // This test class inherits BasicSetupTest class, where the browser is initialized
 // browser variable is available here as it's inherited, so you'll have it available at any place
 public class SeleniumTestngTest extends BasicSetupTest {
 
+    private final String URL = "https://the-internet.herokuapp.com/";
+
+
     @Test
     public void abTestingPageHasSpecificTextTest() {
-        browser.get("https://the-internet.herokuapp.com/");
-        // Write your code here (just an example provided)
-        WebElement abTestingTaskLink = browser.findElement(By.linkText("A/B Testing"));
-        abTestingTaskLink.click();
+        browser.get(URL);
+        browser.findElement(By.linkText("A/B Testing"))
+                .click();
+        WebElement header = browser.findElement(By.cssSelector("div.example h3"));
+        String text = header.getText();
+        Assert.assertTrue(text.equals("A/B Test Control") || text.equals("A/B Test Variation"), "The text not found");
 
-
-        // Write your assertions in the after the steps of scenario are executed to validate results
-        Assert.assertTrue(true);
     }
 
-    // Write the rest of TEST METHODS according to the task here, each method checking one scenario described in README.md file
-    // In the end you should have a set of test methods each of them describing some specific scenario
+    @Test
+    public void addRemoveElementsTest() {
+        browser.get(URL);
+        browser.findElement(By.linkText("Add/Remove Elements"))
+                .click();
+        WebElement addElement = browser.findElement(By.cssSelector("button[onclick='addElement()']"));
+        for (int i = 0; i < 3; i++) {
+            addElement.click();
+        }
+        List<WebElement> deleteElements = browser.findElements(By.cssSelector("button[onclick='deleteElement()']"));
+        Assert.assertEquals(deleteElements.size(), 3, "There should be 3 buttons.");
+
+        for (WebElement deleteElement : deleteElements) {
+            deleteElement.click();
+        }
+
+        deleteElements = browser.findElements(By.cssSelector("button[onclick='deleteElement()']"));
+        Assert.assertEquals(deleteElements.size(), 0, "The buttons Delete should be removed.");
+
+    }
+
+    @Test
+    public void checkboxesTest() {
+        browser.get(URL);
+        browser.findElement(By.linkText("Checkboxes"))
+                .click();
+        List<WebElement> checkboxes = browser.findElements(By.cssSelector("input[type='checkbox']"));
+
+        for (WebElement checkbox : checkboxes) {
+            if (!checkbox.isSelected()) {
+                checkbox.click();
+            }
+        }
+        for (WebElement checkbox : checkboxes) {
+            Assert.assertTrue(checkbox.isSelected(), "The checkbox must be selected.");
+        }
+
+    }
+
+    @Test
+    public void dropdownTest() {
+        browser.get(URL);
+        browser.findElement(By.linkText("Dropdown"))
+                .click();
+        WebElement dropdown = browser.findElement(By.id("dropdown"));
+        dropdown.findElement(By.cssSelector("option[value='2']"))
+                .click();
+        WebElement selected = browser.findElement(By.cssSelector("option[value='2']:checked"));
+        Assert.assertNotNull(selected, "The Option 2 should be selected.");
+    }
+
+    @Test
+    public void formAuthenticationTest() {
+        browser.get(URL);
+        browser.findElement(By.linkText("Form Authentication"))
+                .click();
+        browser.findElement(By.cssSelector("#username")).sendKeys("tomsmith");
+        browser.findElement(By.cssSelector("#password")).sendKeys("SuperSecretPassword!");
+        browser.findElement(By.cssSelector("button[type='submit']")).click();
+
+        WebElement actualMessage = browser.findElement(By.id("flash"));
+        Assert.assertTrue(actualMessage.getText().contains("You logged into a secure area!"), "Login not confirmed.");
+
+        WebElement logout = browser.findElement(By.cssSelector("a[href='/logout']"));
+        logout.click();
+
+        String successMessage = browser.findElement(By.cssSelector("#flash")).getText();
+        Assert.assertTrue(successMessage.contains("You logged out of the secure area!"));
+
+
+    }
+    @Test
+    public void dragAndDropTest() {
+        browser.get(URL);
+        browser.findElement(By.linkText("Drag and Drop"))
+                .click();
+        WebElement columnA = browser.findElement(By.cssSelector("#column-a"));
+        WebElement columnB = browser.findElement(By.cssSelector("#column-b"));
+
+        Actions actions = new Actions(browser);
+        actions.dragAndDrop(columnA, columnB).perform();
+        String columnAText = columnA.findElement(By.tagName("header")).getText();
+        Assert.assertTrue(columnAText.equals("B"));
+    }
+
+    @Test public void horizontalSliderTest() {
+        browser.get(URL);
+        browser.findElement(By.linkText("Horizontal Slider")).click();
+
+        WebElement slider = browser.findElement(By.cssSelector("input[type='range']"));
+        WebElement value = browser.findElement(By.id("range"));
+
+        String initialValue = value.getText();
+        Actions actions = new Actions(browser);
+        actions.clickAndHold(slider).moveByOffset(30, 0).release().perform();
+        String newValue = value.getText();
+        Assert.assertNotEquals(initialValue, newValue, "The slider value has not changed.");
+
+    }
 
 }
